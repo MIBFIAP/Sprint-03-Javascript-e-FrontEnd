f17d85159857bce3ad3d6329bc5ca9a39c04012e
MIB <mibfiap@outlook.com>
2023-09-17 15:27:18 -0300

    RM-99215

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Error from './components/Error.jsx'
 import Home from './components/Home.jsx'
-import Produtos from './components/Produtos.jsx'
 import Blog from './components/Blog.jsx'
 import Login from './components/Login.jsx'
 import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
@@ -18,7 +17,6 @@ const router = createBrowserRouter([
     children:[
       {path: '/', element: <Home/>},
       {path: '/mododianoite', element: <ModoDiaNoite/>},
-      {path: '/produtos', element:<Produtos/>},
       {path: '/blog', element: <Blog/>},
       {path: '/login', element: <Login/>},
     ]
